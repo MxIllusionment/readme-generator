aeb5ae8aabbcfb4881e53f5917364df4d7d1be24
Steven Israel <malenchite@gmail.com>
2021-01-30 14:28:57 -0500

    Create initial question prompts and response

--- readme-gen.js
+++ readme-gen.js
@@ -1,9 +1,65 @@
 // Include packages needed for this application
 const fs = require("fs");
-const gmd = require("./utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// An array of questions for user input
+const questions = [
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username"
+  },
+  {
+    type: "input",
+    message: "What is your e-mail address?",
+    name: "email"
+  },
+  {
+    type: "input",
+    message: "What is the name of your project?",
+    name: "title"
+  },
+  {
+    type: "list",
+    message: "What license would you like to use?",
+    name: "license",
+    choices: ["BSD 3-Clause", "GPL v3", "ISC", "MIT", "Unlicense", "None"],
+    filter: answer => {
+      if (answer === "None") {
+        return "";
+      }
+      return answer;
+    }
+  },
+  {
+    type: "input",
+    message: "Enter a brief description of your project: ",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "What command should be run to install?",
+    name: "installCmd",
+    default: "npm install"
+  },
+  {
+    type: "input",
+    message: "What command should be run for testing?",
+    name: "testCmd",
+    default: "npm test"
+  },
+  {
+    type: "input",
+    message: "Are there any special usage instructions?",
+    name: "usage"
+  },
+  {
+    type: "input",
+    message: "Are there any special instructions for contributing?",
+    name: "contributing"
+  }
+];
 
 // A function to write README file
 function writeToFile(fileName, data) {
@@ -12,8 +68,19 @@ function writeToFile(fileName, data) {
   });
 }
 
-// TODO: Create a function to initialize app
-function init() { }
+// A function to initialize app
+function init() {
+  var dir = './output';
+
+  // Create output directory if it doesn't already exist
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
+  inquirer.prompt(questions).then(answers => {
+    writeToFile(dir + "/README.md", generateMarkdown(answers));
+  });
+}
 
 // Function call to initialize app
 init();
